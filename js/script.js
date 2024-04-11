9566574a54a5d13ab69338f88099129b937eec27
Kiril Tsonev <kirill.tsonev.work@gmail.com>
2024-04-11 04:20:50 +0300

    fix: timer

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ window.addEventListener("DOMContentLoaded", function () {
 
   tabs(".tabheader__item", ".tabcontent", ".tabheader__items", "tabheader__item_active");
   modal("[data-modal]", ".modal", modalTimerId);
-  timer(".timer", "2027-09-20");
+  timer(".timer", "2027-12-20");
   cards();
   calc();
   forms("form", modalTimerId);
